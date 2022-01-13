dd36e82d12bbdcdbabe56a187205f63e578f6a6e
haruka kizawa <s20c1037ga@s.chibakoudai.jp>
2022-01-13 10:12:38 +0000

    add LED emission

--- myled.c
+++ myled.c
@@ -20,7 +20,13 @@ static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_
 	char c;
 	if(copy_from_user(&c,buf,sizeof(char)))
 		return -EFAULT;
-	printk(KERN_INFO "receive %c\n",c);
+
+//	printk(KERN_INFO receive %c\n,c);
+
+	if(c == '0')
+		gpio_base[10] = 1<< 25;
+	else if(c == '1')
+		gpio_base[7] = 1 << 25;
 	return 1;
 }
 
@@ -47,7 +53,7 @@ static struct file_operations led_fops ={
 static int __init init_mod(void)
 {
 	int retval;
-	gpio_base = ioremap_nocache(0x3f200000, 0xA0);
+
 	retval = alloc_chrdev_region(&dev, 0, 1, "myled");
 	if(retval < 0){
 	printk(KERN_ERR "alloc_chrdev_region failed. \n");
@@ -68,6 +74,12 @@ static int __init init_mod(void)
 	}
 	device_create(cls, NULL, dev, NULL, "myled%d",MINOR(dev));
 
+	gpio_base = ioremap_nocache(0x3f200000, 0xA0);
+	const u32 led = 25;
+	const u32 index = led/10;
+	const u32 shift = (led%10)*3;
+	const u32 mask = ~(0x7 << shift);
+	gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
 	return 0;
 }
 
--- myled.mod.c
+++ myled.mod.c
@@ -29,17 +29,17 @@ __used __section(__versions) = {
 	{ 0x68b6ba91, "class_destroy" },
 	{ 0x15311333, "device_destroy" },
 	{ 0xf56f7280, "cdev_del" },
+	{ 0x6b4b2933, "__ioremap" },
+	{ 0xb2ead97c, "kimage_vaddr" },
 	{ 0x515dba02, "device_create" },
 	{ 0x8630fa3a, "__class_create" },
 	{ 0xb049b9b6, "cdev_add" },
 	{ 0x45a81214, "cdev_init" },
 	{ 0xe3ec2f2b, "alloc_chrdev_region" },
-	{ 0x6b4b2933, "__ioremap" },
-	{ 0xb2ead97c, "kimage_vaddr" },
 	{ 0x6cbbfc54, "__arch_copy_to_user" },
+	{ 0xc5850110, "printk" },
 	{ 0xdecd0b29, "__stack_chk_fail" },
 	{ 0xd697e69a, "trace_hardirqs_on" },
-	{ 0xc5850110, "printk" },
 	{ 0x12a4e128, "__arch_copy_from_user" },
 	{ 0xec3d2e1b, "trace_hardirqs_off" },
 	{ 0x3d8560e4, "cpu_hwcaps" },
@@ -51,4 +51,4 @@ __used __section(__versions) = {
 MODULE_INFO(depends, "");
 
 
-MODULE_INFO(srcversion, "EB7A397567C94368C4BDB60");
+MODULE_INFO(srcversion, "4A5BABDD75BA67E414A9B3A");
